22a545edcf97af8e0f512d0235b4ab0b062e25dc
Kshitij Sahdev <85095194+Kshitij-Sahdev@users.noreply.github.com>
2025-11-24 08:25:30 +0530

    Add files via upload

---
+++ realtimedetection.py
@@ -0,0 +1,36 @@
+from keras.models import load_model
+import cv2
+import numpy as np
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(script_dir, "signlanguagedetectionmodel48x48.keras")
+model = load_model(model_path)
+
+def extract_features(image):
+    feature = np.array(image)
+    feature = feature.reshape(1,48,48,1)
+    return feature/255.0
+
+cap = cv2.VideoCapture(0)
+label = ['A', 'M', 'N', 'S', 'T', 'blank']
+while True:
+    _,frame = cap.read()
+    cv2.rectangle(frame,(0,40),(300,300),(0, 165, 255),1)
+    cropframe=frame[40:300,0:300]
+    cropframe=cv2.cvtColor(cropframe,cv2.COLOR_BGR2GRAY)
+    cropframe = cv2.resize(cropframe,(48,48))
+    cropframe = extract_features(cropframe)
+    pred = model.predict(cropframe) 
+    prediction_label = label[pred.argmax()]
+    cv2.rectangle(frame, (0,0), (300, 40), (0, 165, 255), -1)
+    if prediction_label == 'blank':
+        cv2.putText(frame, " ", (10, 30),cv2.FONT_HERSHEY_SIMPLEX,1, (255, 255, 255),2,cv2.LINE_AA)
+    else:
+        accu = "{:.2f}".format(np.max(pred)*100)
+        cv2.putText(frame, f'{prediction_label}  {accu}%', (10, 30),cv2.FONT_HERSHEY_SIMPLEX,1, (255, 255, 255),2,cv2.LINE_AA)
+    cv2.imshow("output",frame)
+    cv2.waitKey(27)
+    
+cap.release()
+cv2.destroyAllWindows()
